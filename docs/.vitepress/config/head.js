e2f52adbcd7d76e6a5f1dd0af29832e69e821d05
zhiyiYo <1319158137@qq.com>
2025-02-20 11:44:02 +0800

    更新 HEAD

--- docs/.vitepress/config/head.js
+++ docs/.vitepress/config/head.js
@@ -4,6 +4,7 @@ const headConfig = [
     // ['meta', { name: 'msapplication-TileColor', content: '#818CF8' }],
     // ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#818CF8' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
+    ['meta', { name: 'algolia-site-verification', content: '8921700CA4D3D4E9' }],
     ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
     ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     ['link', { rel: 'apple-touch-icon', type: 'image/x-icon', sizes: '180x180', href: '/favicon.ico' }],
